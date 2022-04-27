4c92029ba66b74f16e8c8911d1c28a58437bd367
eskoviak <eskoviak@eskoviak.com>
2022-04-26 19:55:24 -0500

    Update from desktop vs code

--- examples/readfile.cxx
+++ examples/readfile.cxx
@@ -4,9 +4,15 @@
 #include <string>
 using namespace std;
 
-int main () {
+int main (int argc, char** argv) {
+  if(argc < 2)
+  {
+    std::cout << "Usage:  readfile <filename>" << std::endl;
+    std::exit(-1);
+  }
+
   string line;
-  ifstream myfile ("example.txt");
+  ifstream myfile (argv[1]);
   if (myfile.is_open())
   {
     while ( getline (myfile,line) )
@@ -16,7 +22,7 @@ int main () {
     myfile.close();
   }
 
-  else cout << "Unable to open file"; 
+  else std::cout << "Unable to open file " << argv[1] << std::endl; 
 
   return 0;
 }
